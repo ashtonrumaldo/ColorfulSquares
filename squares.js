4a03833a85d5098012e3085b1ff6b3fdda84137a
ashtonrumaldo <ashtonrumaldo@gmail.com>
2025-11-12 10:18:40 -0500

    Random Square Position

--- squares.js
+++ squares.js
@@ -1,11 +1,13 @@
 window.onload = function () {
     var squarearea = document.getElementById("squarearea");
 
-    var squareCount = parseInt(math.Random() * 21) + 30
+    var squareCount = parseInt(Math.random() * 21) + 30
 
     for (let i = 0; i < squareCount; i++) {
         var newSquare = document.createElement('div');
         newSquare.className = "square";
+        newSquare.style.left = parseInt(Math.random() * 651) + "px";
+        newSquare.style.top = parseInt(Math.random() * 251) + "px";
         squarearea.appendChild(newSquare);
     }
 }
